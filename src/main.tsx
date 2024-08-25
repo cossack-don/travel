1ad431bd09b3235e048ad4eb7ed50387a25ecefb
cossack-done <ser90593696.zhuravlev@yandex.ru>
2024-08-25 19:20:38 +0300

    add RTK store

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,15 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 
+import { store } from './store'
+import { Provider } from 'react-redux'
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  // <StrictMode>
+      <Provider store={store}>
       <BrowserRouter>
           <App />
       </BrowserRouter>
-  </StrictMode>,
+      </Provider>
+  // </StrictMode>,
 )
--- src/pages/One.tsx
+++ src/pages/One.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import { Counter } from '../feat/counter/Counter';
 export default function One() {
     return (
         <div>
+            <Counter/>
             Start <Link to="/one">one</Link>
         </div>
     );
