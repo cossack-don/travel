063bf777c29c34001e6901e4fd5b407ff23e965e
cossack-done <ser90593696.zhuravlev@yandex.ru>
2024-08-29 20:33:43 +0300

    feat: add pkg flex-box-grid

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,7 +1,7 @@
 import '@/app//style/reset.css'
 import '@/app/style/palette.css'
 import '@/app/style/index.css'
-
+import 'flexboxgrid/dist/flexboxgrid.min.css'
 import StoreProvider from '@/app/providers/store'
 import Router from "@/app/providers/routers";
 import DefaultLayout from "@/app/providers/layouts/DefaultLayout";
