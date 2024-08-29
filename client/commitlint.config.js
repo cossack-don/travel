e697de6f72ac5395d0c97dff291dfc233978ad83
cossack-done <ser90593696.zhuravlev@yandex.ru>
2024-08-29 20:56:36 +0300

    f

--- client/commitlint.config.js
+++ client/commitlint.config.js
@@ -1,6 +1,1 @@
-export default {
-  "extends": ["@commitlint/config-conventional"],
-  "rules": {
-    "body-max-line-length": [0, "always"]
-  }
-};
+export default { extends: ['@commitlint/config-conventional'] };
