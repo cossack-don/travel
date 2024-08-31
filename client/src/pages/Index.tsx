2e1b7870a00005fca8b0cc33b5a30e40cf91c54d
cossack-done <ser90593696.zhuravlev@yandex.ru>
2024-08-31 16:14:02 +0300

    chore: test front+back up

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,5 +1,12 @@
 import { Link } from "react-router-dom";
+import {useEffect} from "react";
+
 const Index = ()=>{
+
+    //TODO для теста бекенда что работает
+    useEffect(()=>{
+        fetch('http://127.0.0.1:8000/days/3').then(res=>res.json()).then(res => console.log(res,333))
+    },[])
 
     return(
         <div className="row between-xs">
