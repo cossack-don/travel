50fcd96e21549092e9194688680543f8e3ceda62
cossack-done <ser90593696.zhuravlev@yandex.ru>
2024-09-22 21:16:19 +0300

    build: test

--- telegram-bot.js
+++ telegram-bot.js
@@ -1,3 +1,33 @@
 const test = process.env;
+const {TG_BOT_TOKEN,TG_CHAT_ID} = process.env;
 
 console.log('message TEST', test?.TG_BOT_TOKEN,test?.TG_CHAT_ID);
+
+const sendMessage = async (message) => {
+    await fetch(`https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage`, {
+        method: "POST",
+        body: JSON.stringify({
+            chat_id: TG_CHAT_ID,
+            parse_mode: "html",
+            text: message,
+        }),
+        headers: { "Content-Type": "application/json" },
+    })
+        .then((v) => v.json())
+        .then(console.log, console.log);
+};
+
+
+const onNewMergeRequest = async () => {
+    await sendMessage(
+        `📣 New MR: [<a href="#">test msg link</a>]`
+    );
+};
+
+const [, , event] = process.argv;
+
+const eventHandlersMap = {
+    newMr: onNewMergeRequest,
+};
+
+(async () => await eventHandlersMap[event]())();
