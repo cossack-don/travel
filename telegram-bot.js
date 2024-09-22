f222d10afef5d133edf0436425a903106a9400b2
cossack-done <ser90593696.zhuravlev@yandex.ru>
2024-09-22 21:11:31 +0300

    build: test

--- telegram-bot.js
+++ telegram-bot.js
@@ -1,3 +1,3 @@
 const test = process.env;
 
-console.log('message TEST', test?.TG_BOT_TOKEN,test?.TG_CHAT_ID,test?.TG_BOT_TOKEN.TG_BOT_TOKEN);
+console.log('message TEST', test?.TG_BOT_TOKEN,test?.TG_CHAT_ID);
