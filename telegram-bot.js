73dfe67821030f0c94708192e4c031e2e6b951a4
cossack-done <ser90593696.zhuravlev@yandex.ru>
2024-09-23 21:51:08 +0300

    build: test

--- telegram-bot.js
+++ telegram-bot.js
@@ -26,13 +26,11 @@ const sendMessage = async (message) => {
 
 const listEvents = {
     MERGED_PR_TO_DEV_BRANCH:{
-        title:'Merged PR in dev branch',
-        bodyMessage:`📣 ${this?.MERGED_PR_TO_DEV_BRANCH?.title} от ${GITHUB_ACTOR} в проект [<a href="https://github.com/cossack-don/travel">CheckList</a>]`
+        bodyMessage:`📣 Merged PR in dev branch от ${GITHUB_ACTOR} в проект [<a href="https://github.com/cossack-don/travel">CheckList</a>]`
 
     },
     OPEN_PR:{
-        title:'New PR in dev branch',
-        bodyMessage:`📣 ${this?.OPEN_PR?.title} от ${GITHUB_ACTOR} в проект [<a href="https://github.com/cossack-don/travel">CheckList</a>]`
+        bodyMessage:`📣 New PR in dev branch от ${GITHUB_ACTOR} в проект [<a href="https://github.com/cossack-don/travel">CheckList</a>]`
     }
 }
 
