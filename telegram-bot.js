758b82e288d1ce9b02ea01388687264794d8dbba
cossack-done <ser90593696.zhuravlev@yandex.ru>
2024-09-23 21:28:52 +0300

    build: test

--- telegram-bot.js
+++ telegram-bot.js
@@ -1,9 +1,13 @@
-const test = process.env;
-const {TG_BOT_TOKEN,TG_CHAT_ID, IS_CREATE_PULL_REQUEST, IS_MERGED_PULL_REQUEST} = process.env;
 
-console.log('message TEST',TG_BOT_TOKEN,TG_CHAT_ID, typeof IS_CREATE_PULL_REQUEST, typeof IS_MERGED_PULL_REQUEST);
+const {TG_BOT_TOKEN,TG_CHAT_ID, IS_CREATE_PULL_REQUEST, IS_MERGED_PULL_REQUEST, GITHUB_ACTOR} = process.env;
+
+const isCreatePullRequest = Boolean(IS_CREATE_PULL_REQUEST)
+const isMergedPullRequest = Boolean(IS_MERGED_PULL_REQUEST)
+
+console.log('message TEST',TG_BOT_TOKEN,TG_CHAT_ID, typeof IS_CREATE_PULL_REQUEST, typeof IS_MERGED_PULL_REQUEST, GITHUB_ACTOR);
 const g = process
 console.log('all-list-process-logs',g)
+
 const sendMessage = async (message) => {
     await fetch(`https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage`, {
         method: "POST",
@@ -19,18 +23,18 @@ const sendMessage = async (message) => {
 };
 
 
-
-
-
-// sendMessage(
-//     `📣 New MR: [<a href="#">test msg link</a>]`
-// );
+sendMessage(
+    `📣 New MR: [<a href="https://github.com/cossack-don/travel">test-link</a>] isStatus-test, от 'name-test'. <a href="#">[open]</a>`
+);
 // const onNewMergeRequest = async () => {
-//     await sendMessage(
-//         `📣 New MR: [<a href="#">test msg link</a>]` Пример сообщения PR merged success in dev branch - author-nickName - url Repo
-//     );
-// };
 //
+//     await sendMessage(
+//         `📣 New MR: [<a href="#">test msg link</a>]` );
+//
+//     //Пример сообщения PR merged success in dev branch - author-nickName - url Repo
+//
+// };
+
 // const [, , event] = process.argv;
 //
 // const eventHandlersMap = {
