7d4d54a4b2504a42a41ad58996afd751e70e4994
cossack-done <ser90593696.zhuravlev@yandex.ru>
2024-11-05 20:07:21 +0300

    feat: part api transport

--- client/src/shared/api/transport/categories-check-list/index.ts
+++ client/src/shared/api/transport/categories-check-list/index.ts
@@ -3,8 +3,18 @@ import { Methods, getBaseURL, ADAPTER_API_REQUEST } from "@/shared/api/axios"
 export const serviceCategoriesCheckList = {
 	getAll: () => {
 		return ADAPTER_API_REQUEST({
-			url: getBaseURL("/types_of_clothes"),
+			url: getBaseURL("/categories"),
 			method: Methods.GET
 		})
 	}
+	// create: ({ name, description }: { name: string; description: string }) => {
+	// 	return ADAPTER_API_REQUEST({
+	// 		url: getBaseURL("/categories"),
+	// 		method: Methods.POST,
+	// 		data: {
+	// 			name,
+	// 			description
+	// 		}
+	// 	})
+	// }
 }
--- client/src/shared/api/transport/index.ts
+++ client/src/shared/api/transport/index.ts
@@ -2,5 +2,12 @@ import { serviceApp } from "@/shared/api/transport/app"
 import { serviceCheckList } from "@/shared/api/transport/check-list"
 import { serviceBlog } from "@/shared/api/transport/blog"
 import { serviceCategoriesCheckList } from "@/shared/api/transport/categories-check-list"
+import { serviceItemsCategoriesCheckList } from "@/shared/api/transport/items-categories-check-list"
 
-export { serviceApp, serviceCheckList, serviceBlog, serviceCategoriesCheckList }
+export {
+	serviceApp,
+	serviceCheckList,
+	serviceBlog,
+	serviceCategoriesCheckList,
+	serviceItemsCategoriesCheckList
+}
